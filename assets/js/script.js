53e279612a4b1654ddf3c8a5a840542b572d5eeb
Logan Merchant <merchantclogan@gmail.com>
2020-08-20 16:19:55 -0600

    add loadTasks() to pull tasks from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -313,6 +313,58 @@ var saveTasks = function() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
+var loadTasks = function() {
+    // Gets task items from localStorage.
+    tasks = localStorage.getItem('tasks');
+    console.log(tasks);
+
+    // Sets `tasks` as an empty arry if there is not saved data in localStorage.
+    if (!tasks) {
+        tasks = [];
+        return false;
+    };
+
+    // Converts tasks from JSON stringify, to an array of objects.
+    tasks = JSON.parse(tasks);
+    console.log(tasks);
+
+    // Iterates through the tasks array and creates the elements on the page.
+    for (var i = 0; i < tasks.length; i++) {
+        tasks[i].id = taskIdCounter;
+
+        var listItemEl = document.createElement('li');
+        listItemEl.className = 'task-item';
+        listItemEl.setAttribute('data-task-id', tasks[i].id);
+        listItemEl.setAttribute('draggable', 'true');
+
+        var taskInfoEl = document.createElement('div');
+        taskInfoEl.className = 'task-info';
+        taskInfoEl.innerHTML = '<h3 class="task-name">' + tasks[i].name + '</h3><span class="task-type">' + tasks[i].type + '</span>';
+
+        listItemEl.appendChild(taskInfoEl);
+
+        var taskActionsEl = createTaskActions(tasks[i].id);
+
+        listItemEl.appendChild(taskActionsEl);
+
+        if (tasks[i].status === 'to do') {
+            listItemEl.querySelector('select[name="status-change"]').selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        } else if (tasks[i].status === 'in progress') {
+            listItemEl.querySelector('select[name="status-change"]').selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        } else if (tasks[i].status === 'complete') {
+            listItemEl.querySelector('select[name="status-change"]').selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        };
+
+        taskIdCounter++;
+        console.log(listItemEl);
+    };
+};
+
+loadTasks();
+
 
 // When formEl elements are submitted, perform the taskFormHandler function.
 formEl.addEventListener("submit", taskFormHandler);
